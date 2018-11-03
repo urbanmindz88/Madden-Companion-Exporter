c4a713ff0f9b4e04cfe1f4bbb310f52537c0d039
urbanmindz88 <44722026+urbanmindz88@users.noreply.github.com>
2018-11-03 19:19:48 +0000

    Update app.js

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ const app = express();
 
 // TODO: Enter the path to your service account json file
 // Need help with this step go here: https://firebase.google.com/docs/admin/setup
-// const serviceAccount = require("./REPLACE_WITH_SERVICE_ACCOUNT.json");
+const serviceAccount = require("./Firebase_Info.json");
 
 // TODO: Enter your database url from firebase
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://<DATABASE_NAME>.firebaseio.com/"
-// });
+admin.initializeApp({
+   credential: admin.credential.cert(serviceAccount),
+   databaseURL: "https://maddenexporter-3a368.firebaseio.com/"
+ });
 
 
 app.set('port', (process.env.PORT || 3001));
